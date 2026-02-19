39929d8ad09a8e98fab1d2326224820f9822b6a9
IanBau24 <ibautistaambr@miners.utep.edu>
2026-02-18 20:44:35 -0700

    got exit keyword to work by comparing strings

--- src/uimain.c
+++ src/uimain.c
@@ -5,9 +5,11 @@ int main(){
     while(1){ // run until break (EOF)
         char currLine[100];
         int c, i = 0;
-        printf("$ ");
+        printf("$");
+        // h e l l o
         while((c = getchar()) != '\n' && c != EOF){ // read each char and append to array if not EOF or \n
-            currLine[i] = c;
+            currLine[i] = c; // h e
+            // h e l l o \n
             i++;
         }
 
@@ -17,11 +19,19 @@ int main(){
         
         currLine[i] = '\0'; // finish current line with string end code
 
-        // ? check why this line doesn't work
-        // if(strcmp(currLine, "exit")) break;
+        // TODO turn this exit check into a function
+        // check for exit
+        char *str = "exit";
+        short match = 1;
+        for(int j = 0; j < i; j++){
+            if(*(currLine + j) != *(str + j)){ // check if they are different
+                match = 0; // change match to false
+                break;
+            }
+            if(*(currLine + j) == '\0') break; // case that currLine ends before exit does
+        }
+        if(match) break; // break if the string passed is exit
 
         printf("%s\n",currLine); // print back out what user typed
     }
-
-
 }
